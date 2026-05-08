8c3bfcee594f8a5eb08ce10e7628ad3e6b8da8a8
santa <santarellihectorm@gmail.com>
2026-05-08 01:37:32 -0300

    usefull indexer

--- src/indexer.c
+++ src/indexer.c
@@ -3,11 +3,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
 
 #include "indexer.h"
 #include "types.h"
 
+
+void cleanAppEntry(AppEntry* entry){
+  if(!entry)return;
+  if(entry->name)free(entry->name);
+  if(entry->exec)free(entry->exec);
+  if(entry->icon)free(entry->icon);
+  entry->name=NULL;
+  entry->exec=NULL;
+  entry->icon=NULL;
+}
+
+void freeAppList(AppList* list){
+  if(!list)return;
+  for(int i=0;i< list->count;i++)cleanAppEntry(&list->apps[i]);
+  if(list->apps)free(list->apps);
+  list->apps=NULL;
+  free(list);
+}
 
 static void growAppList(AppList *list){
   if(!list)abort();
@@ -29,13 +48,22 @@ static void growAppList(AppList *list){
   }
 }
 
+static char* trimString(char* str){
+  size_t len=strlen(str);
+  while(len>0&&isspace((unsigned char)str[len-1])){
+    len--;
+    str[len]='\0';
+  }
+  while(*str&&isspace((unsigned char)*str))str++;
+  return str;
+}
+
 AppList* buildAppList(const char* dirs){
-  AppList* list=malloc(sizeof(AppList));
+  AppList* list=calloc(1,sizeof(AppList));
   if(!list){
     fprintf(stderr,"Error allocating AppList\n");
     abort();
   }
-  list->capacity=0;
   growAppList(list);
   struct dirent *entry;
   DIR* dp=opendir(dirs);
@@ -53,11 +81,36 @@ AppList* buildAppList(const char* dirs){
       fprintf(stderr,"Error openning file %s.\n",fullPath);
       continue;
     }
-    list->count++;
-    if(list->count>=list->capacity)growAppList(list);
+    AppEntry tmp={0};
+    bool inSection=false,skip=true;
     char buffer[512];
     while(fgets(buffer,sizeof(buffer),deskFile)){
-      //TODO: parsing
+      char* line=trimString(buffer);
+      if(line[0]=='\0'||line[0]=='#'||line[0]==';')continue;
+      len=strlen(line);
+      if(strncmp(line,"[Desktop Entry]",15)==0)inSection=true;
+      else if(line[0]=='[')inSection=false;
+      if(inSection){
+        if(strncmp(line,"NoDisplay=true",14)==0){
+          skip=true;
+          break;
+        }else if(!tmp.name&&strncmp(line,"Name=",5)==0){
+          tmp.name=strdup(line+5);
+        }else if(!tmp.exec&&strncmp(line,"Exec=",5)==0){
+          tmp.exec=strdup(line+5);
+        }else if(!tmp.icon&&strncmp(line,"Icon=",5)==0){
+          tmp.icon=strdup(line+5);
+        }else if(strncmp(line,"Type=Application",16)==0){
+          skip=false;
+        }
+      }
+    }
+    if(skip||!tmp.name||!tmp.exec){
+      cleanAppEntry(&tmp);
+    }else{
+      list->apps[list->count]=tmp;
+      list->count++;
+      if(list->count>=list->capacity)growAppList(list);
     }
     fclose(deskFile);
   }
--- src/indexer.h
+++ src/indexer.h
@@ -3,6 +3,8 @@
 
 #include "types.h"
 
+void cleanAppEntry(AppEntry* entry);
+void freeAppList(AppList* list);
 AppList* buildAppList(const char* dirs);
 
 #endif
